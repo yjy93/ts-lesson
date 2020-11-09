77da0ac9c23b0ddfcea38fec8348ccb81820e681
Gene <Genejob@163.com>
2020-11-09 21:23:42 +0800

    ts 中的装饰器

---
+++ src/5. ts中类的使用.ts
@@ -0,0 +1,54 @@
+/**
+ * @author: Gene
+ * @age: 永远18岁的美少年
+ * @Email： Genejob@163.com
+ * @date: 2020-11-08 22:25:24
+ * @description: 类 ES6 类
+ */
+
+// as 断言成 XXX类型
+// ! 非空断言
+// ? 链式判断运算符  有值取值, 没有值 返回 undefined
+
+// ES6 类:=> 静态属性  私有的实例属性  共享的原型属性
+class Pointer {
+    x!: number; // 表示实例上有这个属性 非空断言
+    y!: number; // 非空断言
+
+    constructor(x: number, y?: number, ...args: number[]) { // 这些参数, 函数中的使用方式,这里都可以使用.
+        this.x = x;
+        this.y = y as number;
+    }
+}
+
+let pointer = new Pointer(1, 2, 3, 4, 5, 6);
+console.log(pointer.x);
+
+// --------------------------------------------------------------------
+// public 表示 父类本身 子类, 外部 都可以获取这个属性
+// protected 受保护的, 父类可以访问到, 子类可以访问到, 但是外部不能访问
+// private 只有自己能够访问到该属性
+// 如果 constructor 被标识成了 private 或者 protected, 则此类不能被new,
+// 被标识成了private, 则不能被子类继承
+class Animal {
+    public name!: string;
+    public age!: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Cat extends Animal {
+    address = '';
+
+    constructor(name: string, age: number, address: string) {
+        super(name, age);
+        this.address = address
+    }
+}
+
+let tom = new Cat('miao', 10, '北京');
+
+export {}
--- src/index.ts
+++ src/index.ts
@@ -2,53 +2,62 @@
  * @author: Gene
  * @age: 永远18岁的美少年
  * @Email： Genejob@163.com
- * @date: 2020-11-08 22:25:24
- * @description: 类 ES6 类
+ * @date: 2020-11-09 20:06:44
+ * @description: 装饰器
  */
 
-// as 断言成 XXX类型
-// ! 非空断言
-// ? 链式判断运算符  有值取值, 没有值 返回 undefined
-
-// ES6 类:=> 静态属性  私有的实例属性  共享的原型属性
-class Pointer {
-    x!: number; // 表示实例上有这个属性 非空断言
-    y!: number; // 非空断言
-
-    constructor(x: number, y?: number, ...args: number[]) { // 这些参数, 函数中的使用方式,这里都可以使用.
-        this.x = x;
-        this.y = y as number;
+// 类装饰器
+function Modifier(target: Function) { // 修饰类本身,当前参数就是类
+    target.prototype.say = function () {
+        console.log('say');
     }
 }
 
-let pointer = new Pointer(1, 2, 3, 4, 5, 6);
-console.log(pointer.x);
+// 类属性的装饰器
+function toUppercase1(target: any, key: string) { // target 指的是类的原型 key指的是属性
+    let value = target[key];
+    Object.defineProperty(target, key, {
+        get() {
+            return value.toUpperCase()
+        },
+        set(newVal) {
+            value = newVal
+        }
+    })
+}
 
-// --------------------------------------------------------------------
-// public 表示 父类本身 子类, 外部 都可以获取这个属性
-// protected 受保护的, 父类可以访问到, 子类可以访问到, 但是外部不能访问
-// private 只有自己能够访问到该属性
-// 如果 constructor 被标识成了 private 或者 protected, 则此类不能被new,
-// 被标识成了private, 则不能被子类继承
-class Animal {
-    public name!: string;
-    public age!: number;
+function double(target: any, key: string) { // 这个 target 就是类
+    let value = target[key];
+    Object.defineProperty(target, key, {
+        get() {
+            return value * 2
+        },
+        set(newVal) {
+            value = newVal
+        }
+    })
+}
 
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
+// 原型上方法的 装饰器
+function Enum(target: any, key: string, descriptor: PropertyDescriptor) {
+    // descriptor: value configurable writbale enumberable
+    descriptor.enumerable = false
+}
+
+// 扩展属性 和 方法
+@Modifier
+class Person {
+    say!: Function
+    @toUppercase1 // 属性装饰器
+    name: string = 'Gene'
+    @double // 类的静态属性装饰器
+    static age: number = 10
+
+    @Enum // 修饰原型上的方法
+    getName() {
     }
 }
 
-class Cat extends Animal {
-    address = '';
-
-    constructor(name: string, age: number, address: string) {
-        super(name, age);
-        this.address = address
-    }
-}
-
-let tom = new Cat('miao', 10, '北京');
-
+let person = new Person();
+console.log(person);
 export {}
