e99a872ad255ea97184d9c927cc4932a51eda37e
Gene <Genejob@163.com>
2020-12-10 20:05:01 +0800

    ts 的类型

--- src/1.ts中的数据类型.ts
+++ src/1.ts中的数据类型.ts
@@ -6,7 +6,7 @@
  * @description: TS 中拥有的 类型
  */
 
-//1. 基础类型
+//1. 基础类型: => 数字, 字符串 布尔类型
 let num: number = 10;
 let str: string = 'hello'
 let bool: boolean = true
@@ -20,43 +20,52 @@ tuple.push('杨阳');
 let arr1: number[] = [1, 2, 3]
 let arr2: string[] = ['Gene'];
 
-// 4. 联合类型 可以看做并集, 可以使用联合的类型其中的.
+// 4. 联合类型 可以看做并集, 可以使用联合的类型其中的任意一种类型
 let arr3: (string | number)[] = ['Gene', 1, 2, 3];
 
 
 //5. 枚举类型
-enum USER_ROLE {
+/** ---- 异构枚举 ---- */
+enum USER_ROLE {// 异构枚举, 可以在枚举中放不同类型.
     USER = 'a', //默认下标是从0 开始
     ADMIN = 0,
     MANAGER,
 }
 
 // 默认可以正向取出, 也可以反取
+
 // 异构枚举, 可以在枚举中放不同的类型, 可以通过数字 自动向下推断
 // console.log(USER_ROLE.USER, USER_ROLE[0]);
 
+/** ----- 常量枚举 ------- */
 // 常量枚举 只是提供了一个类型
 const enum USER_ROLE2 { // 更语义化
-    USER,
+    USER, // 默认下标是从 0 开始
     ADMIN,
 }
 
 console.log(USER_ROLE2.USER);
 
-// any 类型, 不进行类型检测, 相当于没写类型
+
+/** ------------- any 类型 ---------- */
+// any 类型, 不进行类型检测, 相当于没写类型.
 let arr: any = ['字符串', 123, true];
 
-// null 和 undefined 类型
+/** ------------- null 和 undefined 类型 ---------- */
+// null 和 undefined 类型 是任何类型的子类型.
 let str2: number | string;
 // str2 = undefined // 非严格模式才可以
 
-// void 空类型 只能接收 null 和 undefined
+
+/** ======== void 空类型 ===================== */
+// void 空类型 只能接收 null 和 undefined. 一般用来函数的返回值
 // let v: void
 // v = null // 默认在严格模式下,不能将 null 赋给 void,非严格模式才可以
 
+/** ============ never 类型 =================== */
 // never 类型 永远不, 是任何类型的子类型, 可以把 never 赋给任何类型
-// 永远达不到的情况有三种
-//1. 出错误 2. 死循环 3. 类型判断时 会出现 never
+// 永远达不到的情况有三种.
+// 1. 出错误 2. 死循环 3. 类型判断时 会出现 never
 function MyError(): never {
     throw new Error("err")
 }
